8e401de46324f5f502d66a15f8f5e6b73c635911
rakshit4550 <rakshitramani99@gmail.com>
2025-05-12 17:34:30 +0530

    add changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,9 @@ import "./App.css";
 import Login from "./pages/user/Login";
 import AdminHome from "./pages/admin/AdminHome";
 import Whitelabel from "./pages/admin/Whitelabel";
-<<<<<<< HEAD
 import ProofManagement from "./pages/admin/Markettype";
 import SportsManagement from "./pages/admin/SportsManagement";
-=======
 import ProofType from "./pages/admin/ProofType";
->>>>>>> 78da85c57eb085160a6a5a59b1918f2381aa3733
 
 function App() {
   return (
@@ -18,12 +15,9 @@ function App() {
         <Route path="/admin" element={<Login />}></Route>
         <Route path="/adminhome" element={<AdminHome />} />
         <Route path="/whitelabel" element={<Whitelabel />} />
-<<<<<<< HEAD
         <Route path="/proofs" element={<ProofManagement />} />
         <Route path="/sports" element={<SportsManagement />} />
-=======
         <Route path="/proofs" element={<ProofType />} />
->>>>>>> 78da85c57eb085160a6a5a59b1918f2381aa3733
         {/* user Routes */}
         <Route path="/" element={<Login />} />
       </Routes>
